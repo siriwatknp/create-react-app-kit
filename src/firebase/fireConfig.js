273a40a69cc619a7a4da84750fe55783945af3fe
Siriwat Kunaporn <siriwatkunaporn@gmail.com>
2019-01-19 19:53:01 +0700

    remove firebase config credential

--- src/firebase/fireConfig.js
+++ src/firebase/fireConfig.js
@@ -1,13 +1,6 @@
 import firebase from 'firebase';
 
-const developmentConfig = {
-  apiKey: 'AIzaSyCOEUP0WenLSE0T-cjRNa6m7oqMSI6tq3M',
-  authDomain: 'react-app-ce8ba.firebaseapp.com',
-  databaseURL: 'https://react-app-ce8ba.firebaseio.com',
-  projectId: 'react-app-ce8ba',
-  storageBucket: 'react-app-ce8ba.appspot.com',
-  messagingSenderId: '552804122338',
-};
+const developmentConfig = {};
 const stagingConfig = developmentConfig;
 const productionConfig = {};
 const config = {
